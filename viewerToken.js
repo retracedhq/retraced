4d030afd07e35ad2fdfac8ff98cc2ba517d1ff1e
Marc Campbell <marc.e.campbell@gmail.com>
2016-10-13 19:27:48 -0700

    What

--- viewerToken.js
+++ viewerToken.js
@@ -1,5 +1,41 @@
 'use strict';
 
+const validateApiToken = require('./lib/security/validateApiToken');
+const createViewerToken = require('./lib/models/viewertoken/create');
+const checkAccess = require('./lib/security/checkAccess');
+
 module.exports.default = (event, context, cb) => {
-  cb(new Error('[500] test'));
+  let apiToken;
+  validateApiToken(event)
+  .then((t) => {
+    apiToken = t;
+    return checkAccess({
+      api_token: apiToken,
+      project_id: event.path.projectId,
+    });
+  })
+  .then((valid) => {
+    if (!valid) {
+      cb(new Error('[401] Unauthorized'));
+      return;
+    }
+
+    return createViewerToken({
+      project_id: event.path.projectId,
+      environment_id: apiToken.environment_id,
+      team_id: event.query.team_id,
+      format: event.query.output ? event.query.output : 'json',
+    });
+  })
+  .then((viewerToken) => {
+    const result = {
+      token: viewerToken,
+    };
+
+    cb(null, result);
+    return;
+  })
+  .catch((err) => {
+    cb(err);
+  });
 };
