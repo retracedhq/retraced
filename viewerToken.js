172bb394c9fad1c49bb3090482404620224e1e5d
Marc Campbell <marc.e.campbell@gmail.com>
2016-10-13 19:23:38 -0700

    OMG

--- viewerToken.js
+++ viewerToken.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const validateApiToken = require('./lib/security/validateApiToken');
-const createViewerToken = require('./lib/models/viewertoken/create');
-const checkAccess = require('./lib/security/checkAccess');
-
 module.exports.default = (event, context, cb) => {
-  cb(null, {token: '123'});
+  cb(new Error('[500] test'));
 };
