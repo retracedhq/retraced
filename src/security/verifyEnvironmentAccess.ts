51be4b4da6ff2e4eda916975bc8ea154625b8289
Dex <dexter.i.horthy@gmail.com>
2017-04-18 17:21:25 -0700

    appease code climate

--- src/security/verifyEnvironmentAccess.ts
+++ src/security/verifyEnvironmentAccess.ts
@@ -3,12 +3,12 @@ import getPgPool from "../persistence/pg";
 const pgPool = getPgPool();
 
 export interface Options {
-    environmentId: string;
-    userId: string;
+  environmentId: string;
+  userId: string;
 }
 
-export default async function (opts: Options): Promise<boolean> {
-    const q = "select count(1) from environmentuser where user_id = $1 and environment_id = $2";
-    const result = await pgPool.query(q, [opts.userId, opts.environmentId]);
-    return result.rows[0].count > 0;
+export default async function verifyEnvironmentAccess(opts: Options): Promise<boolean> {
+  const q = "select count(1) from environmentuser where user_id = $1 and environment_id = $2";
+  const result = await pgPool.query(q, [opts.userId, opts.environmentId]);
+  return result.rows[0].count > 0;
 }
