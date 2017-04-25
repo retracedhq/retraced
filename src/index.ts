006190725db5913326b14a812b387ed808c92976
Dex <dexter.i.horthy@gmail.com>
2017-04-25 10:08:49 -0700

    try killing catch-all handler

--- src/index.ts
+++ src/index.ts
@@ -76,16 +76,6 @@ function buildRoutes() {
     register(route, handler, app);
   });
 
-  ["get", "post", "delete", "put"].forEach((method) => {
-    app[method]("*", (req, res, next) => {
-      if (!res.statusCode) {
-        const errMsg = `Route not found for ${req.method} ${req.originalUrl}`;
-        console.log(chalk.red(`[${req.ip}] ${errMsg}`));
-        res.status(404).send(errMsg);
-      }
-    });
-  });
-
 }
 
 function serve() {
