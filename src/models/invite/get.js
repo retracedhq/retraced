f2b26be63a56764f2e9052bc2a1c09a5cb4db74a
Dex <dexter.i.horthy@gmail.com>
2017-06-01 15:41:06 -0700

    Fix error where `pg` is not defined on `pg.release()`
    This means an error in pg.connect(), but the error gets swallowed
    because the `finally` block tries to call `release()`
    
    ```
    [_login:edf631c4] !! 500 TypeError: Cannot read property 'release' of
    undefined
        at Object.default_1 [as default]
    (/src/src/models/user/getByExternalAuth.js:20:7)
        at process._tickDomainCallback (internal/process/next_tick.js:135:7)
    ```

--- src/models/invite/get.js
+++ src/models/invite/get.js
@@ -3,31 +3,23 @@ import getPgPool from "../../persistence/pg";
 const pgPool = getPgPool();
 
 export default async function getInvite(opts) {
-  let pg;
-  try {
-    pg = await pgPool.connect();
-
-    const fields = `id, email, project_id,
+  const fields = `id, email, project_id,
       extract(epoch from created) * 1000 as created`;
 
-    let q;
-    let v;
-    if (opts.inviteId) {
-      q = `select ${fields} from invite where id = $1`;
-      v = [opts.inviteId];
-    } else if (opts.email) {
-      q = `select ${fields} from invite where email = $1`;
-      v = [opts.email];
-    }
-
-    const result = await pg.query(q, v);
-    if (result.rowCount > 0) {
-      return result.rows[0];
-    }
-
-    return null;
-
-  } finally {
-    pg.release();
+  let q;
+  let v;
+  if (opts.inviteId) {
+    q = `select ${fields} from invite where id = $1`;
+    v = [opts.inviteId];
+  } else if (opts.email) {
+    q = `select ${fields} from invite where email = $1`;
+    v = [opts.email];
   }
+
+  const result = await pgPool.query(q, v);
+  if (result.rowCount > 0) {
+    return result.rows[0];
+  }
+
+  return null;
 }
