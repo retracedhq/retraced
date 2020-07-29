d99f578320187ef9dd1fc5c5faa5855ca8b89685
Andrew Lavery <laverya@umich.edu>
2020-07-29 17:02:47 -0400

    Revert "statically include es migration"

--- src/_db/commands/up/es.ts
+++ src/_db/commands/up/es.ts
@@ -1,12 +1,13 @@
 import "source-map-support/register";
 import * as chalk from "chalk";
+import * as walk from "walk";
+import * as path from "path";
 import * as util from "util";
 import * as bugsnag from "bugsnag";
 
 import getElasticsearch from "../../persistence/elasticsearch";
 import getPgPool from "../../persistence/pg";
 import { setupBugsnag } from "../../common";
-import * as template from "../../../../migrations/es/template"
 
 setupBugsnag();
 
@@ -34,6 +35,10 @@ export const builder = {
   },
 };
 
+function getSchemaPath() {
+  return path.join(__dirname, "../../../../migrations/es");
+}
+
 export const handler = (argv) => {
   const es = getElasticsearch();
   const pgPool = getPgPool();
@@ -45,67 +50,92 @@ export const handler = (argv) => {
       console.log(chalk.red(util.inspect(err)));
       process.exit(1);
     }
+    const walker = walk.walk(getSchemaPath(), {
+      followLinks: false,
+    });
 
-    const esQuery = template();
+    walker.on("file", (root, stat, next) => {
+      if (path.extname(stat.name) !== ".js") {
+        next();
+        return;
+      }
 
-    pg.query(`create table if not exists es_migration_meta (
-      id int primary key,
-      created timestamp
-    )`)
-      .then(() => {
-        return pg.query("select * from es_migration_meta where id = $1", [esQuery.time]);
-      })
-      .then((result) => {
-        if (result.rowCount > 0) {
-          console.log(chalk.dim(`${esQuery.time} already completed`));
-          return Promise.resolve(false);
-        }
+      const tokens = stat.name.split("-");
+      const timestamp = Number(tokens[0]);
+      const name = tokens[1].slice(0, -3);
 
-        return new Promise<any>((resolve, reject) => {
-          const esQuery = template();
-          switch (esQuery.category) {
-            default:
-              reject(new Error("Unknown category"));
-              break;
+      pg.query(`create table if not exists es_migration_meta (
+        id int primary key,
+        created timestamp
+      )`)
+        .then(() => {
+          return pg.query("select * from es_migration_meta where id = $1", [timestamp]);
+        })
+        .then((result) => {
+          if (result.rowCount > 0) {
+            console.log(chalk.dim(`${timestamp} ${name}`));
+            return Promise.resolve(false);
+          }
 
-            case "indices": {
-              switch (esQuery.op) {
-                default:
-                  reject(new Error("Unknown operation"));
-                  break;
+          return new Promise<any>((resolve, reject) => {
+            const esQuery = require(path.join(getSchemaPath(), stat.name))();
+            switch (esQuery.category) {
+              default:
+                reject(new Error("Unknown category"));
+                break;
 
-                case "putTemplate": {
-                  es.indices.putTemplate(esQuery.params, (err2, resp) => {
-                    if (err2) {
-                      reject(err2);
-                      return;
-                    }
-                    console.log(chalk.green(`migration ${esQuery.time} completed`));
-                    resolve(true);
-                  });
+              case "indices": {
+                switch (esQuery.op) {
+                  default:
+                    reject(new Error("Unknown operation"));
+                    break;
+
+                  case "putTemplate": {
+                    es.indices.putTemplate(esQuery.params, (err2, resp) => {
+                      if (err2) {
+                        reject(err2);
+                        return;
+                      }
+                      console.log(chalk.green(stat.name));
+                      resolve(true);
+                    });
+                  }
                 }
               }
             }
+          });
+        })
+        .then(<any> ((shouldSave) => {
+          if (shouldSave) {
+            return pg.query(`insert into es_migration_meta (
+            id, created
+          ) values (
+            $1, now()
+          ) on conflict do nothing`, [timestamp]);
           }
+          return Promise.resolve();
+        }))
+        .then(() => {
+          next(); // done! next file pls
+        })
+        .catch((err2) => {
+          bugsnag.notify(err2);
+          console.log(chalk.red(err2.stack));
+          process.exit(1);
         });
-      })
-      .then(<any> ((shouldSave) => {
-        if (shouldSave) {
-          return pg.query(`insert into es_migration_meta (
-          id, created
-        ) values (
-          $1, now()
-        ) on conflict do nothing`, [esQuery.time]);
-        }
-        return Promise.resolve();
-      }))
-      .catch((err2) => {
-        bugsnag.notify(err2);
-        console.log(chalk.red(err2.stack));
-        process.exit(1);
-      })
-      .then(() => {
-        process.exit(0);
-      })
+    });
+
+    walker.on("errors", (root, stat, next) => {
+      stat.forEach((n) => {
+        bugsnag.notify(n.error);
+        console.error(`[ERROR] ${n.name}`);
+        console.error(n.error.message || `${n.error.code}:${n.error.path}`);
+      });
+      next();
+    });
+
+    walker.on("end", () => {
+      process.exit(0);
+    });
   });
 };
