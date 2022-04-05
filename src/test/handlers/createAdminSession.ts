361a3ae3342fce3971b688d03731351bb3398ad6
Utkarsh Mehta <ukrocks.mehta@gmail.com>
2022-04-05 17:31:58 +0530

    partial coverage for createAdminSession

---
+++ src/test/handlers/createAdminSession.ts
@@ -0,0 +1,45 @@
+import { suite, test } from "mocha-typescript";
+import { expect } from "chai";
+import * as TypeMoq from "typemoq";
+import * as moment from "moment";
+
+import * as pg from "pg";
+
+import handler from "../../handlers/createAdminSession";
+import { createSession, ExternalAuth, CreateSessionResult } from "../../handlers/createAdminSession";
+
+@suite class CreateAdminSessionTest {
+    @test public async "CreateAdminSession#handler() throws if external_auth is not present in boby"() {
+        
+        let req = {
+            body: {}
+        };
+        const expected = new Error("Missing required auth");
+
+        try {
+            await handler(req);
+            throw new Error(`Expected error ${expected} to be thrown`);
+        } catch (err) {
+            expect(err.err.message).to.equal(expected.message);
+            expect(err.status).to.equal(400);
+        }
+    }
+
+    @test public async "CreateAdminSession#handler() throws if auth0 is not initialised"() {
+        
+        let req = {
+            body: {
+                external_auth: ""
+            }
+        };
+        const expected = new Error("Missing required auth");
+
+        try {
+            await handler(req);
+            throw new Error(`Expected error ${expected} to be thrown`);
+        } catch (err) {
+            expect(err.err.message).to.equal(expected.message);
+            expect(err.status).to.equal(400);
+        }
+    }
+}
