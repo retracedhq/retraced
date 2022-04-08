6bb3945e53f1d1cd495c6cb12553aecde2291f50
Utkarsh Mehta <ukrocks.mehta@gmail.com>
2022-04-08 13:03:05 +0530

    deleteEnterpriseToken test added

---
+++ src/test/handlers/deleteEnterpriseToken.ts
@@ -0,0 +1,70 @@
+import { suite, test } from "mocha-typescript";
+import { deleteEnterpriseToken } from "../../handlers/deleteEnterpriseToken";
+import getPgPool from "../../persistence/pg";
+import { createEnterpriseToken } from "../../handlers/createEnterpriseToken";
+import { expect } from "chai";
+import { AdminTokenStore } from "../../models/admin_token/store";
+import create from "../../models/api_token/create";
+import { result } from "lodash";
+
+@suite class DeleteEnterpriseToken {
+    @test public async "deleteEnterpriseToken#deleteEnterpriseToken()"() {
+        let pool = getPgPool();
+        try {
+            await cleanup(pool);
+            await setup(pool);
+            let result = await createEnterpriseToken(`token=test`, "test", "test", {
+                display_name: "test",
+            });
+            await deleteEnterpriseToken("token=test", "test", "test", result.token);
+            expect(1).to.equal(1);
+        } catch (ex) {
+            console.log(ex);
+        } finally {
+            await cleanup(pool);
+        }
+    }
+    @test public async "deleteEnterpriseToken#deleteEnterpriseToken() throws if token is wrong"() {
+        let pool = getPgPool();
+        try {
+            await cleanup(pool);
+            await setup(pool);
+            let result = await createEnterpriseToken(`token=test`, "test", "test", {
+                display_name: "test",
+            });
+            await deleteEnterpriseToken("token=test", "test", "test", result.token + "1");
+            console.log(result);
+            throw new Error(`Expected error 'Not Found' to be thrown`);
+        } catch (ex) {
+            expect(ex.status).to.equal(404);
+            expect(ex.err.message).to.equal("Not Found");
+        } finally {
+            await cleanup(pool);
+        }
+    }
+}
+async function setup(pool) {
+    await pool.query("INSERT INTO project (id, name) VALUES ($1, $2)", ["test", "test"]);
+    await pool.query("INSERT INTO environment (id, name, project_id) VALUES ($1, $2, $3)", ["test", "test", "test"]);
+    await pool.query("INSERT INTO retraceduser (id, email) VALUES ($1, $2)", ["test", "test@test.com"]);
+    await pool.query("INSERT INTO environmentuser (user_id, environment_id, email_token) VALUES ($1, $2, $3)", ["test", "test", "dummytoken"]);
+    await pool.query("INSERT INTO projectuser (id, project_id, user_id) VALUES ($1, $2, $3)", ["test", "test", "test"]);
+    let res = await AdminTokenStore.default().createAdminToken("test");
+    await create("test", "test", {
+        name: "test",
+        disabled: false,
+    }, undefined, "test");
+    return res;
+}
+
+async function cleanup(pool) {
+    await pool.query(`DELETE FROM admin_token WHERE user_id=$1`, ["test"]);
+    await pool.query(`DELETE FROM environmentuser WHERE user_id=$1`, ["test"]);
+    await pool.query(`DELETE FROM environment WHERE name=$1`, ["test"]);
+    await pool.query(`DELETE FROM project WHERE name=$1 OR name=$2`, ["test", "test1"]);
+    await pool.query(`DELETE FROM projectuser WHERE project_id=$1`, ["test"]);
+    await pool.query(`DELETE FROM token WHERE environment_id=$1`, ["test"]);
+    await pool.query(`DELETE FROM retraceduser WHERE email=$1`, ["test@test.com"]);
+    await pool.query(`DELETE FROM eitapi_token WHERE environment_id=$1`, ["test"]);
+}
+
