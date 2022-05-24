9be90dd4c14d16b6c16bdd2aad5749ecf7da3fcb
Utkarsh Mehta <ukrocks.mehta@gmail.com>
2022-05-24 15:13:10 +0530

    minor fix

--- src/commands/bootstrap.ts
+++ src/commands/bootstrap.ts
@@ -1,96 +1,98 @@
 import { bootstrapProject } from "../headless";
 import util from "util";
 import getPgPool from "../persistence/pg";
-import { forEach } from "lodash";
 
 export const name = "bootstrap";
 export const describe =
-  "Bootstrap a retraced project with a specified projectId, environmentId, and apiKey";
+    "Bootstrap a retraced project with a specified projectId, environmentId, and apiKey";
 export const builder = {
-  projectId: {
-    demand: true,
-  },
-  environmentId: {
-    demand: true,
-  },
-  apiKey: {
-    demand: true,
-  },
-  projectName: {
-    demand: true,
-  },
-  environmentName: {
-    default: "default",
-  },
-  tokenName: {
-    default: "default",
-  },
+    projectId: {
+        demand: true,
+    },
+    environmentId: {
+        demand: true,
+    },
+    apiKey: {
+        demand: true,
+    },
+    projectName: {
+        demand: true,
+    },
+    environmentName: {
+        default: "default",
+    },
+    tokenName: {
+        default: "default",
+    },
 };
 
 async function checkTableAvailability(tableName) {
-  let pool = getPgPool();
-  if (pool) {
-    try {
-      var sql = "SELECT * FROM " + tableName + " LIMIT 1";
-      await pool.query(sql, async function (err) {
-        if (err) {
-          console.log("error in checkTableAvailability while executing query");
-          await executeQuery(tableName);
+    let result = false;
+    do {
+        let pool = getPgPool();
+        if (pool) {
+            try {
+                console.log(`Checking for table ${tableName}`);
+                result = await executeQuery(tableName);
+                console.log(`Result => ${result}`);
+            } catch (ex) {
+                result = false;
+                console.log(ex);
+            }
         }
-        console.log("query executed successfuly");
-      });
-    } catch (ex) {
-      console.log(ex);
-    }
-  }
+    } while (!result);
 }
 
-async function executeQuery(tableName) {
-  let pool = getPgPool();
-  try {
-    var sql = "SELECT * FROM " + tableName + " LIMIT 1";
-    await pool.query(sql, async function (err) {
-      if (err) {
-        console.log("error in executeErrorQuery while executing query");
-        await executeQuery(tableName);
-      }
-      console.log("query executed successfuly");
-    });
-  } catch (ex) {
-    console.log(ex);
-  }
+async function executeQuery(tableName) : Promise<boolean> {
+    return new Promise((resolve) => {
+        let pool = getPgPool();
+        try {
+            var sql = "SELECT * FROM " + tableName + " LIMIT 1";
+            pool.query(sql, async function (err) {
+                if (!err) {
+                    console.log("query executed successfuly");
+                    resolve(true);
+                }
+                console.log(`Table ${tableName} not found!`);
+                resolve(false);
+            });
+        } catch (ex) {
+            console.log(`Table ${tableName} not found!`);
+            resolve(false);
+        }
+    })
 }
 
 export const handler = async (argv) => {
-  const {
-    projectId,
-    apiKey,
-    environmentId,
-    projectName,
-    environmentName,
-    tokenName,
-  } = argv;
+    const {
+        projectId,
+        apiKey,
+        environmentId,
+        projectName,
+        environmentName,
+        tokenName,
+    } = argv;
 
-  let tables = ["project", "environment", "token"];
-  await tables.forEach(checkTableAvailability);
+    let tables = ["project", "environment", "token"];
+    await tables.forEach(checkTableAvailability);
 
-  bootstrapProject({
-    projectId,
-    apiKey,
-    environmentId,
-    projectName,
-    environmentName,
-    tokenName,
-    keyVarRef: "apiKey",
-    projectVarRef: "projectId",
-    envVarRef: "environmentId",
-  })
-    .then(() => {
-      console.log(`bootstrapped project ${argv.projectId}`);
-      process.exit(0);
+    bootstrapProject({
+        projectId,
+        apiKey,
+        environmentId,
+        projectName,
+        environmentName,
+        tokenName,
+        keyVarRef: "apiKey",
+        projectVarRef: "projectId",
+        envVarRef: "environmentId",
     })
-    .catch((err) => {
-      console.log(util.inspect(err));
-      process.exit(1);
-    });
+        .then(() => {
+            console.log(`bootstrapped project ${argv.projectId}`);
+            process.exit(0);
+        })
+        .catch((err) => {
+            console.log(util.inspect(err));
+            process.exit(1);
+        });
 };
