a850567b791fe39820fa78a4239e37fafed82904
ukrocks007 <ukrocks.mehta@gmail.com>
2023-01-03 19:00:41 +0530

    added pagination to list projects

--- src/models/project/list.ts
+++ src/models/project/list.ts
@@ -1,25 +1,34 @@
-
 import getPgPool from "../../persistence/pg";
 
 const pgPool = getPgPool();
 
 export interface Options {
   user_id: string;
+  page: string;
+  count: string;
 }
 
 /**
  * Asynchronously returns all projects for a user from the database
  */
 export default async function listProjects(opts: Options) {
-  const q = `select project.* from project
+  const q =
+    opts.page && opts.count
+      ? `select project.* from project
+    inner join projectuser
+    on project.id = projectuser.project_id
+    where projectuser.user_id = $1
+    order by project.created desc offset $2 limit $3`
+      : `select project.* from project
     inner join projectuser
     on project.id = projectuser.project_id
     where projectuser.user_id = $1`;
-  const v = [
-    opts.user_id,
-  ];
+  const v =
+    opts.page && opts.count
+      ? [opts.user_id, Number(opts.page) > 1 ? (Number(opts.page) - 1) * Number(opts.count) : 0, Number(opts.count)]
+      : [opts.user_id];
 
   const result = await pgPool.query(q, v);
 
-  return result.rowCount > 0 ? result.rows : [] ;
+  return result.rowCount > 0 ? result.rows : [];
 }
--- src/models/project/listall.ts
+++ src/models/project/listall.ts
@@ -2,13 +2,24 @@ import getPgPool from "../../persistence/pg";
 
 const pgPool = getPgPool();
 
+export interface Options {
+  page: string;
+  count: string;
+}
+
 /**
  * Asynchronously returns all projects for a user from the database
  */
-export default async function listAllProjects() {
-  const q = `select project.* from project`;
-
-  const result = await pgPool.query(q);
+export default async function listAllProjects(opts: Options) {
+  const q =
+    opts.page && opts.count
+      ? `select project.* from project order by created desc offset $1 limit $2`
+      : `select project.* from project`;
+  const v =
+    opts.page && opts.count
+      ? [Number(opts.page) > 1 ? (Number(opts.page) - 1) * Number(opts.count) : 0, Number(opts.count)]
+      : [];
+  const result = await pgPool.query(q, v);
 
   return result.rowCount > 0 ? result.rows : [];
 }
