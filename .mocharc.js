f1f187abce1235a9c231a9c94aa98151e0ff33c4
Deepak Prabhakara <deepak@boxyhq.com>
2023-01-09 14:18:18 +0000

    enabled --enable-source-maps flag when running node

--- .mocharc.js
+++ .mocharc.js
@@ -2,5 +2,6 @@ module.exports = {
   require: ["ts-node/register", "mocha-jenkins-reporter"],
   colors: true,
   exit: true,
+  "enable-source-maps": true,
   spec: ["src/test/**/*.ts", "src/**/*_test.ts"],
 };
