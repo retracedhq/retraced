4f6cfe3d671b3fdbeab45854541e04aa2076a1be
ukrocks007 <ukrocks.mehta@gmail.com>
2023-01-10 18:55:30 +0530

    page command elasticsearch upgrade

--- src/_db/commands/reindex/shared/page.ts
+++ src/_db/commands/reindex/shared/page.ts
@@ -4,131 +4,129 @@ import util from "util";
 import ProgressBar from "progress";
 import { mapValues } from "../../../../common/mapper";
 
-import getElasticsearch from "../../../persistence/elasticsearch";
+import { getNewElasticsearch } from "../../../persistence/elasticsearch";
 import { Event } from "../../../persistence/EventSource";
 import common from "../../../common";
 import { logger } from "../../../../logger";
+import { Client } from "@elastic/elasticsearch";
 
 let totalIndexed: number = 0;
 
-export const makePageIndexer =
-  (writeIndex: string) => async (result: Event[]) => {
-    logger.info(`processing page with count ${result.length}`);
-    const es = getElasticsearch();
-    const pbar = new ProgressBar("[:bar] :percent :etas", {
-      incomplete: " ",
-      width: 40,
-      total: result.length,
-    });
-    const promises = result.map(async (row: Event) => {
-      let actor;
-      if (row.actor_id) {
-        actor = await common.getActor(row.actor_id);
-      }
-
-      let target;
-      if (row.object_id) {
-        // -_-
-        target = await common.getTarget(row.object_id);
-      }
-
-      // postgres you're killing me here with the naming
-      const group: any = await common.getGroup(row.team_id);
-
-      // Rename field group_id => id. If the group is from the cache, it may already have been fixed, so we have to check.
-      if (group && !group.id) {
-        group.id = group.group_id;
-        _.unset(group, "group_id");
-      }
-
-      let indexableEvent: any = _.pick(row, [
-        "id",
-        "description",
-        "action",
-        "crud",
-        "is_failure",
-        "is_anonymous",
-        "created",
-        "received",
-        "source_ip",
-        "country",
-        "loc_subdiv1",
-        "loc_subdiv2",
-        "raw",
-        "canonical_time",
-      ]);
-
-      indexableEvent = mapValues(indexableEvent);
-
-      indexableEvent.group = mapValues(group);
-
-      indexableEvent.actor = mapValues(actor);
-
-      indexableEvent.target = mapValues(target);
-
-      pbar.tick(1);
-
-      return indexableEvent;
-    });
-
-    const toBeIndexedDirty = await Promise.all(promises);
-    const toBeIndexed = toBeIndexedDirty.filter((o) => o);
-
-    // Bulk index
-    pbar.terminate();
-    console.log();
-    if (_.isEmpty(toBeIndexed)) {
-      console.log(chalk.yellow("No valid rows to index!"));
-      return;
+export const makePageIndexer = (writeIndex: string) => async (result: Event[]) => {
+  logger.info(`processing page with count ${result.length}`);
+  const newEs: Client = getNewElasticsearch();
+  const pbar = new ProgressBar("[:bar] :percent :etas", {
+    incomplete: " ",
+    width: 40,
+    total: result.length,
+  });
+  const promises = result.map(async (row: Event) => {
+    let actor;
+    if (row.actor_id) {
+      actor = await common.getActor(row.actor_id);
     }
 
-    const body: any[] = [];
-    for (const eventToIndex of toBeIndexed) {
-      if (!eventToIndex) {
-        continue;
-      }
-
-      [eventToIndex.actor, eventToIndex.target].forEach((obj) => {
-        if (obj.foreign_id) {
-          obj.id = obj.foreign_id;
-        }
-      });
-
-      body.push({
-        index: {
-          _index: writeIndex,
-          _type: "_doc",
-        },
-      });
-      body.push(eventToIndex);
+    let target;
+    if (row.object_id) {
+      // -_-
+      target = await common.getTarget(row.object_id);
     }
 
-    logger.info(`indexing page with size ${result.length}`);
-    totalIndexed += result.length;
-    await new Promise<void>((resolve, reject) => {
-      es.bulk({ body }, (errr, resp) => {
-        if (errr) {
-          console.log(chalk.red(errr.stack));
-          process.exit(1);
-        }
+    // postgres you're killing me here with the naming
+    const group: any = await common.getGroup(row.team_id);
 
-        if (resp.errors) {
-          _.forEach(resp.items, (item) => {
-            _.forIn(item, (innerItem) => {
-              if (innerItem.error) {
-                console.log(chalk.red(util.inspect(innerItem.error)));
-                console.log(util.inspect(innerItem.error, false, 100, true));
-              }
-            });
+    // Rename field group_id => id. If the group is from the cache, it may already have been fixed, so we have to check.
+    if (group && !group.id) {
+      group.id = group.group_id;
+      _.unset(group, "group_id");
+    }
+
+    let indexableEvent: any = _.pick(row, [
+      "id",
+      "description",
+      "action",
+      "crud",
+      "is_failure",
+      "is_anonymous",
+      "created",
+      "received",
+      "source_ip",
+      "country",
+      "loc_subdiv1",
+      "loc_subdiv2",
+      "raw",
+      "canonical_time",
+    ]);
+
+    indexableEvent = mapValues(indexableEvent);
+
+    indexableEvent.group = mapValues(group);
+
+    indexableEvent.actor = mapValues(actor);
+
+    indexableEvent.target = mapValues(target);
+
+    pbar.tick(1);
+
+    return indexableEvent;
+  });
+
+  const toBeIndexedDirty = await Promise.all(promises);
+  const toBeIndexed = toBeIndexedDirty.filter((o) => o);
+
+  // Bulk index
+  pbar.terminate();
+  console.log();
+  if (_.isEmpty(toBeIndexed)) {
+    console.log(chalk.yellow("No valid rows to index!"));
+    return;
+  }
+
+  const body: any[] = [];
+  for (const eventToIndex of toBeIndexed) {
+    if (!eventToIndex) {
+      continue;
+    }
+
+    [eventToIndex.actor, eventToIndex.target].forEach((obj) => {
+      if (obj.foreign_id) {
+        obj.id = obj.foreign_id;
+      }
+    });
+
+    body.push({
+      index: {
+        _index: writeIndex,
+        _type: "_doc",
+      },
+    });
+    body.push(eventToIndex);
+  }
+
+  logger.info(`indexing page with size ${result.length}`);
+  totalIndexed += result.length;
+  await new Promise<void>((resolve, reject) => {
+    newEs.bulk({ body }, (errr, resp) => {
+      if (errr) {
+        console.log(chalk.red(errr.stack));
+        process.exit(1);
+      }
+
+      if (resp.body.errors) {
+        _.forEach(resp.body.items, (item) => {
+          _.forIn(item, (innerItem) => {
+            if (innerItem.error) {
+              console.log(chalk.red(util.inspect(innerItem.error)));
+              console.log(util.inspect(innerItem.error, false, 100, true));
+            }
           });
-          console.log(
-            chalk.red("Errors returned by bulk op, unable to continue")
-          );
-          process.exit(1);
-        }
+        });
+        console.log(chalk.red("Errors returned by bulk op, unable to continue"));
+        process.exit(1);
+      }
 
-        logger.info(`finished index, ${totalIndexed} completed so far`);
-        resolve();
-      });
+      logger.info(`finished index, ${totalIndexed} completed so far`);
+      resolve();
     });
-  };
+  });
+};
