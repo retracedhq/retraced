9813ae25c674eda5435ccc7132a0520d886e8440
Deepak Prabhakara <deepak@boxyhq.com>
2023-01-19 12:32:27 +0000

    added prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  trailingComma: "es5",
+  semi: true,
+  printWidth: 110,
+  arrowParens: "always",
+  importOrderSeparation: true,
+};
