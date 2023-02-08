469331e59036888d0e9501e3ccf43a5804d7cce5
Deepak Prabhakara <deepak@boxyhq.com>
2023-02-08 09:14:52 +0000

    added en vars to turn off analytics

--- src/_processor/healthz.ts
+++ src/_processor/healthz.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { logger } from "./logger";
 import { AnalyticsController } from "./analytics";
+import config from "../config";
 
 const app = express();
 let lastNSQ: Date = new Date();
 
 export async function startHealthz() {
-  await new AnalyticsController().init();
+  if (!config.RETRACED_NO_ANALYTICS) {
+    console.info(
+      "Anonymous analytics enabled. You can disable this by setting the DO_NOT_TRACK=1 or RETRACED_NO_ANALYTICS=1 environment variables"
+    );
+
+    await new AnalyticsController().init();
+  }
 
   // Needed for Kubernetes health checks
   app.get("/healthz", (req, res) => {
