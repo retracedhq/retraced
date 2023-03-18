bd694c6249d20d6bbbb60bdccf569186e4863dbc
Deepak Prabhakara <deepak@boxyhq.com>
2023-03-18 01:35:08 +0000

    lint

--- src/models/event/canonicalize.ts
+++ src/models/event/canonicalize.ts
@@ -1,10 +1,8 @@
-import _ from "lodash";
-
 import Event from "./";
 import { EventInternal, computeHash } from "@retracedhq/retraced";
 
 // Produces a canonical hash string representation of an event.
 // See the Swagger spec for more details.
 export default function (event: Event): string {
-  return computeHash(event as EventInternal, event.id!).hashResult;
+  return computeHash(event as EventInternal, event.id as string).hashResult;
 }
