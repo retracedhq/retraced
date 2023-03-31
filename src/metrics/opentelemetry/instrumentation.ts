fa4ba22ab0fb6ba2efcce4081897665e43e6d89e
Aswin V <vaswin91@gmail.com>
2023-03-31 16:24:44 +0545

    Align otel init with jackson

--- src/metrics/opentelemetry/instrumentation.ts
+++ src/metrics/opentelemetry/instrumentation.ts
@@ -4,36 +4,6 @@ import otel, {
   type Attributes,
   type Counter,
 } from "@opentelemetry/api";
-import { MeterProvider, PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
-import { Resource } from "@opentelemetry/resources";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
-import config from "../../config";
-
-if (process.env.NODE_ENV !== "test") {
-  const resource = Resource.default().merge(
-    new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: "retraced",
-      // [SemanticResourceAttributes.SERVICE_VERSION]: "0.1.0",
-    })
-  );
-
-  const metricExporter = new OTLPMetricExporter();
-
-  const metricReader = new PeriodicExportingMetricReader({
-    exporter: metricExporter,
-    exportIntervalMillis: Number(config.OTLP_INTERVAL_MILLIS) || 30000,
-  });
-
-  const meterProvider = new MeterProvider({
-    resource,
-  });
-
-  meterProvider.addMetricReader(metricReader);
-
-  // Set this MeterProvider to be global to the app being instrumented.
-  otel.metrics.setGlobalMeterProvider(meterProvider);
-}
 
 let counters;
 let gauges;
