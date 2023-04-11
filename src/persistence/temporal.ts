837dd833b2e126fec4aff8a5e592fc60a58f5caf
Kiran K <mailtokirankk@gmail.com>
2023-04-11 22:03:48 +0530

    remove console.info

--- src/persistence/temporal.ts
+++ src/persistence/temporal.ts
@@ -1,6 +1,5 @@
 import { WorkflowClient, Connection } from "@temporalio/client";
 
-import { logger } from "../logger";
 import config from "../config";
 
 let connection: Connection;
@@ -10,8 +9,6 @@ const createWorkflowClient = async (): Promise<WorkflowClient> => {
     connection = await Connection.connect({
       address: config.TEMPORAL_ADDRESS,
     });
-
-    logger.info("New Temporal connection created");
   }
 
   return new WorkflowClient({ connection });
