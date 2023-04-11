6dfbe13ee2695ea919a220b125bb1faa8ff8ce90
ukrocks007 <ukrocks.mehta@gmail.com>
2023-04-11 21:10:17 +0530

    added logic for deletion from PG

---
+++ src/_processor/workers/cleanupPostgresEvents.ts
@@ -0,0 +1,26 @@
+import config from "../../config";
+import { logger } from "../logger";
+import getPgPool from "../persistence/pg";
+
+const pgPool = getPgPool();
+
+export default async function cleanupPostgres(job: any) {
+  const { projectId, environemntId, beforeTimestamp } = job;
+  // Delete from indexed_events
+  if (config.PG_SEARCH) {
+    const q = `
+    DELETE FROM indexed_events
+      WHERE 
+        project_id = $1 AND
+        environment_id = $2 AND
+        to_timestamp(CAST(doc->>'received' AS BIGINT) / 1000) < $3`;
+    const values = [projectId, environemntId, +new Date(beforeTimestamp)];
+    const result = await pgPool.query(q, values);
+    logger.info(`Old events deleted from indexed_events: ${result.rowCount}`);
+  }
+  // Delete from ingest_tasks
+  const q = `DELETE FROM ingest_tasks WHERE project_id = $1 AND environment_id = $2 AND received < $3`;
+  const values = [projectId, environemntId, new Date(beforeTimestamp)];
+  const result = await pgPool.query(q, values);
+  logger.info(`Old events deleted from ingest_tasks: ${result.rowCount}`);
+}
