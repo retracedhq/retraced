6ca47d7bac1252c80ab46cd71154dbc4a9d74ec9
Kiran K <mailtokirankk@gmail.com>
2023-04-12 12:31:27 +0530

    Wait for namespace to be ready

--- src/_processor/temporal/schedule/start.ts
+++ src/_processor/temporal/schedule/start.ts
@@ -1,6 +1,7 @@
 import type { ScheduleOptions } from "@temporalio/client";
 import { Connection, Client, ScheduleOverlapPolicy } from "@temporalio/client";
 
+import config from "../../../config";
 import {
   ingestFromBacklogWorkflow,
   normalizeRepairWorkflow,
@@ -58,13 +59,49 @@ const schedules: ScheduleOptions[] = [
 ];
 
 async function run() {
+  await waitForNamespaceToBeReady();
+
+  // Wait for namespace cache to be updated
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  const connection = await Connection.connect({
+    address: config.TEMPORAL_ADDRESS,
+  });
+
   const client = new Client({
-    connection: await Connection.connect(),
+    connection,
   });
 
   await Promise.all(schedules.map((schedule) => client.schedule.create(schedule)));
 
   await client.connection.close();
+}
+
+async function waitForNamespaceToBeReady() {
+  const connection = await Connection.connect({
+    address: config.TEMPORAL_ADDRESS,
+  });
+
+  let namespaceExist = false;
+
+  while (!namespaceExist) {
+    try {
+      const result = await connection.workflowService.describeNamespace({
+        namespace: "default",
+      });
+
+      if (result) {
+        console.info("Namespace is ready.");
+        namespaceExist = true;
+        break;
+      }
+    } catch (error) {
+      console.error("Namespace is not ready yet. Retrying in 5 seconds...");
+    }
+
+    // Wait before trying again
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  }
 }
 
 run().catch((err) => {
