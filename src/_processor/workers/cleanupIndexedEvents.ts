ac6d51aa9f8c0b7b9eb90ba70d76d93936712805
ukrocks007 <ukrocks.mehta@gmail.com>
2023-04-14 17:12:09 +0530

    fixes

--- src/_processor/workers/cleanupIndexedEvents.ts
+++ src/_processor/workers/cleanupIndexedEvents.ts
@@ -7,34 +7,39 @@ import getPgPool from "../persistence/pg";
 const pgPool = getPgPool();
 
 export default async function cleanupIndexedEvents(job: any) {
-  const { projectId, environemntId, beforeTimestamp } = job;
-  // Delete from indexed_events
-  if (!config.PG_SEARCH) {
-    // Delee from elasticsearch by query with timestamp less than beforeTimestamp
-    const alias = `retraced.${projectId}.${environemntId}.current`;
-    const es: Client = getESWithoutRetry();
-    const result = await es.deleteByQuery({
-      index: alias,
-      body: {
-        query: {
-          bool: {
-            must: [{ range: { received: { lt: +beforeTimestamp } } }],
+  try {
+    const { projectId, environemntId, beforeTimestamp } = job;
+    // Delete from indexed_events
+    if (!config.PG_SEARCH) {
+      // Delee from elasticsearch by query with timestamp less than beforeTimestamp
+      const alias = `retraced.${projectId}.${environemntId}`;
+      const es: Client = getESWithoutRetry();
+      const result = await es.deleteByQuery({
+        index: alias,
+        body: {
+          query: {
+            bool: {
+              must: [{ range: { received: { lt: +beforeTimestamp } } }],
+            },
           },
         },
-      },
-    });
-    logger.info(`Old events deleted from elasticsearch: ${result.body.deleted}}`);
-    return result.body.deleted;
-  } else {
-    const q = `
+      });
+      logger.info(`Old events deleted from elasticsearch: ${result.body.deleted}}`);
+      return result.body.deleted;
+    } else {
+      const q = `
     DELETE FROM indexed_events
       WHERE 
         project_id = $1 AND
         environment_id = $2 AND
         to_timestamp(CAST(doc->>'received' AS BIGINT) / 1000) < $3`;
-    const values = [projectId, environemntId, new Date(beforeTimestamp)];
-    const res = await pgPool.query(q, values);
-    logger.info(`Old events deleted from indexed_events: ${res.rowCount}`);
-    return res.rowCount;
+      const values = [projectId, environemntId, new Date(beforeTimestamp)];
+      const res = await pgPool.query(q, values);
+      logger.info(`Old events deleted from indexed_events: ${res.rowCount}`);
+      return res.rowCount;
+    }
+  } catch (ex) {
+    logger.error(ex);
+    return 0;
   }
 }
--- src/_processor/workers/cleanupIngestTask.ts
+++ src/_processor/workers/cleanupIngestTask.ts
@@ -6,27 +6,45 @@ const pgPool = getPgPool();
 export default async function cleanupIngestTask(job: any) {
   const { projectId, environemntId, beforeTimestamp, batchSize = 1000 } = job;
   // Delete from ingest_tasks
-  const query = `SELECT * FROM ingest_task WHERE project_id = $1 AND environment_id = $2 AND received < $3`;
+  const query = `SELECT COUNT(*) FROM ingest_task WHERE project_id = $1 AND environment_id = $2 AND received < $3`;
   const events = await pgPool.query(query, [projectId, environemntId, new Date(beforeTimestamp)]);
-  logger.info(`Got events ingest_tasks: ${events.rowCount}`);
-  backupAndDeleteIngestTasks(events, batchSize);
-  return events.rowCount;
+  logger.info(`Got events ingest_tasks: ${events.rows[0].count}`);
+  backupAndDeleteIngestTasks(projectId, environemntId, beforeTimestamp, batchSize);
+  return events.rows[0].count;
 }
 
-export async function backupAndDeleteIngestTasks(events, batchSize = 1000) {
+export async function backupAndDeleteIngestTasks(
+  projectId,
+  environemntId,
+  beforeTimestamp,
+  batchSize = 1000
+) {
   // Delete from ingest_tasks by batch
-  const batches = Math.ceil(events.rowCount / batchSize);
-  for (let i = 0; i < batches; i++) {
-    const batch = events.rows.slice(i * batchSize, (i + 1) * batchSize);
+  let i = 0;
+  do {
+    const query = `SELECT * FROM ingest_task WHERE project_id = $1 AND environment_id = $2 AND received < $3 LIMIT $4 OFFSET $5`;
+    const events = await pgPool.query(query, [
+      projectId,
+      environemntId,
+      new Date(beforeTimestamp),
+      batchSize,
+      i * batchSize,
+    ]);
+    if (events.rowCount === 0) {
+      break;
+    }
+    const batch = events.rows;
     const ids = batch.map((row) => row.id);
     const q = `DELETE FROM ingest_task WHERE id = ANY($1)`;
     const values = [ids];
     const backedup = backupIngestTaskBatch(batch);
-    if (backedup !== ids.length) {
+    if (backedup === ids.length) {
       const res = await pgPool.query(q, values);
       logger.info(`Old events deleted from ingest_tasks: ${res.rowCount}`);
     }
-  }
+    i++;
+  } while (true);
+  return;
 }
 
 export function backupIngestTaskBatch(batch: any[]) {
