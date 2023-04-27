88070e323c3f583fb1c3f70f4660285796c6f62b
Aswin V <vaswin91@gmail.com>
2023-04-27 15:29:46 +0530

    Refactor test and add test for `restoreOriginalEvent`

--- src/_processor/test/workers/NormalizeEventTest.ts
+++ src/_processor/test/workers/NormalizeEventTest.ts
@@ -5,10 +5,146 @@ import normalizeEvent from "../../workers/normalizeEvent";
 import { Pool } from "pg";
 import { NSQClient } from "../../persistence/nsq";
 import { expect } from "chai";
+import { restoreOriginalEvent } from "../../common";
 
 const LOCALHOST_IP = "127.0.0.1";
 const RECEIVED = new Date().getTime();
 
+const ORIGINAL_EVENT = {
+  action: "some.record.created",
+  crud: "c",
+  group: { id: "dev", name: "dev" },
+  created: "2023-01-14T15:48:44.573Z",
+  actor: {
+    id: "sanju@boxyhq.com",
+    name: "sanjusamson",
+    href: "https://rr.com",
+    fields: { coolness: "100/100" },
+  },
+  target: {
+    id: "101",
+    name: "tasks",
+    type: "tasks",
+    href: "https://boxyhq.com",
+    fields: { coolness: "5/5" },
+  },
+  source_ip: LOCALHOST_IP,
+  description: "Some record got created",
+  is_failure: false,
+  is_anonymous: false,
+  component: "crm",
+  version: "v0.0.1",
+  external_id: "EXTERNAL_ID_1",
+  metadata: { sorted: "false", sampled: "true" },
+  fields: { country: "India" },
+};
+
+const GROUP = {
+  project_id: "dev",
+  environment_id: "dev",
+  name: "dev",
+  event_count: 1,
+  created_at: 1682078809046,
+  last_active: 1682078809046,
+  group_id: "dev",
+};
+
+const TARGET = {
+  id: "6415d3ef8bb844e4994f0932fa07639e",
+  environment_id: "dev",
+  event_count: 1,
+  foreign_id: "101",
+  name: "tasks",
+  project_id: "dev",
+  url: "https://boxyhq.com",
+  type: "tasks",
+  fields: { coolness: "5/5" },
+  created: 1682078809053,
+  first_active: 1682078809053,
+  last_active: 1682078809053,
+};
+
+const ACTOR = {
+  id: "78b370701169423b8a733bfae504ad62",
+  environment_id: "dev",
+  event_count: 1,
+  foreign_id: "sanju@boxyhq.com",
+  name: "sanjusamson",
+  project_id: "dev",
+  url: "https://rr.com",
+  fields: { coolness: "100/100" },
+  created: 1682078809050,
+  first_active: 1682078809050,
+  last_active: 1682078809050,
+};
+
+const ACTION = {
+  id: "7c9e3abe9ad442c2b9be60d9e3c52b87",
+  created: "2023-04-21 12:06:49.054",
+  environment_id: "dev",
+  event_count: "1",
+  first_active: "2023-04-21 12:06:49.054",
+  action: "some.record.created",
+  last_active: "2023-04-21 12:10:00.026",
+  project_id: "dev",
+  display_template: null,
+};
+
+const NORMALIZED_EVENT = {
+  action: "some.record.created",
+  component: "crm",
+  created: 1673711324573,
+  crud: "c",
+  description: "Some record got created",
+  is_anonymous: false,
+  is_failure: false,
+  source_ip: LOCALHOST_IP,
+  version: "v0.0.1",
+  id: "925099768bbc42d6adaa9a13d7ac7d6b",
+  received: RECEIVED,
+  canonical_time: 1673711324573,
+  fields: { country: "India" },
+  group: {
+    project_id: "dev",
+    environment_id: "dev",
+    name: "dev",
+    event_count: 1,
+    created_at: 1682078809046,
+    last_active: 1682078809046,
+    id: "dev",
+  },
+  actor: {
+    id: "78b370701169423b8a733bfae504ad62",
+    environment_id: "dev",
+    event_count: 1,
+    foreign_id: "sanju@boxyhq.com",
+    name: "sanjusamson",
+    project_id: "dev",
+    url: "https://rr.com",
+    fields: { coolness: "100/100" },
+    created: 1682078809050,
+    first_active: 1682078809050,
+    last_active: 1682078809050,
+  },
+  target: {
+    id: "6415d3ef8bb844e4994f0932fa07639e",
+    environment_id: "dev",
+    event_count: 1,
+    foreign_id: "101",
+    name: "tasks",
+    project_id: "dev",
+    url: "https://boxyhq.com",
+    type: "tasks",
+    fields: { coolness: "5/5" },
+    created: 1682078809053,
+    first_active: 1682078809053,
+    last_active: 1682078809053,
+  },
+  external_id: "EXTERNAL_ID_1",
+  metadata: { sorted: "false", sampled: "true" },
+};
+
+const COMPRESSED_EVENT = { created: "2023-01-14T15:48:44.573Z" };
 @suite
 class NormalizeEventTest {
   queryStub: sinon.SinonStub<any[], any>;
@@ -30,56 +166,13 @@ class NormalizeEventTest {
   @test
   @params(
     {
-      originalEvent: `{"action":"some.record.created","crud":"c","group":{"id":"dev"},"created":"2023-01-14T15:48:44.573Z","actor":{"id":"sanju@boxyhq.com","name":"sanjusamson","href":"https://rr.com","fields":{"coolness":"100/100"}},"target":{"id":"101","name":"tasks","type":"tasks","href":"https://boxyhq.com","fields":{"coolness":"5/5"}},"source_ip":"${LOCALHOST_IP}","description":"Some record got created","is_failure":false,"is_anonymous":false,"component":"crm","version":"v0.0.1","external_id":"EXTERNAL_ID_1","metadata":{"sorted":"false","sampled":"true"},"fields":{"country":"India"}}`,
-      normalizedEvent: `{"action":"some.record.created","component":"crm","created":1673711324573,"crud":"c","description":"Some record got created","is_anonymous":false,"is_failure":false,"source_ip":"${LOCALHOST_IP}","version":"v0.0.1","id":"925099768bbc42d6adaa9a13d7ac7d6b","received":${RECEIVED},"canonical_time":1673711324573,"fields":{"country":"India"},"group":{"project_id":"dev","environment_id":"dev","name":null,"event_count":1,"created_at":1682078809046,"last_active":1682078809046,"id":"dev"},"actor":{"id":"78b370701169423b8a733bfae504ad62","environment_id":"dev","event_count":1,"foreign_id":"sanju@boxyhq.com","name":"sanjusamson","project_id":"dev","url":"https://rr.com","fields":{"coolness":"100/100"},"created":1682078809050,"first_active":1682078809050,"last_active":1682078809050},"target":{"id":"6415d3ef8bb844e4994f0932fa07639e","environment_id":"dev","event_count":1,"foreign_id":"101","name":"tasks","project_id":"dev","url":"https://boxyhq.com","type":"tasks","fields":{"coolness":"5/5"},"created":1682078809053,"first_active":1682078809053,"last_active":1682078809053},"external_id":"EXTERNAL_ID_1","metadata":{"sorted":"false","sampled":"true"}}`,
-      group: {
-        project_id: "dev",
-        environment_id: "dev",
-        name: null,
-        event_count: 1,
-        created_at: 1682078809046,
-        last_active: 1682078809046,
-        group_id: "dev",
-      },
-      target: {
-        id: "6415d3ef8bb844e4994f0932fa07639e",
-        environment_id: "dev",
-        event_count: 1,
-        foreign_id: "101",
-        name: "tasks",
-        project_id: "dev",
-        url: "https://boxyhq.com",
-        type: "tasks",
-        fields: { coolness: "5/5" },
-        created: 1682078809053,
-        first_active: 1682078809053,
-        last_active: 1682078809053,
-      },
-      actor: {
-        id: "78b370701169423b8a733bfae504ad62",
-        environment_id: "dev",
-        event_count: 1,
-        foreign_id: "sanju@boxyhq.com",
-        name: "sanjusamson",
-        project_id: "dev",
-        url: "https://rr.com",
-        fields: { coolness: "100/100" },
-        created: 1682078809050,
-        first_active: 1682078809050,
-        last_active: 1682078809050,
-      },
-      action: {
-        id: "7c9e3abe9ad442c2b9be60d9e3c52b87",
-        created: "2023-04-21 12:06:49.054",
-        environment_id: "dev",
-        event_count: "1",
-        first_active: "2023-04-21 12:06:49.054",
-        action: "some.record.created",
-        last_active: "2023-04-21 12:10:00.026",
-        project_id: "dev",
-        display_template: null,
-      },
-      compressedEvent: '{"created":"2023-01-14T15:48:44.573Z"}',
+      originalEvent: ORIGINAL_EVENT,
+      normalizedEvent: NORMALIZED_EVENT,
+      group: GROUP,
+      target: TARGET,
+      actor: ACTOR,
+      action: ACTION,
+      compressedEvent: COMPRESSED_EVENT,
     },
     "Should populate normalized_event as expected, clear out original_event and create compressed_event"
   )
@@ -95,7 +188,7 @@ class NormalizeEventTest {
     const rows = [
       {
         id: "100",
-        original_event: originalEvent,
+        original_event: JSON.stringify(originalEvent),
         normalized_event: "",
         saved_to_dynamo: false,
         saved_to_postgres: false,
@@ -169,12 +262,23 @@ class NormalizeEventTest {
       "Failed to query ingest_task table correctly"
     ).to.be.true;
     expect(
-      this.queryStub
-        .getCall(10)
-        .calledWithMatch(normalizeQuery, [JSON.parse(normalizedEvent), JSON.parse(compressedEvent), "100"]),
+      this.queryStub.getCall(10).calledWithMatch(normalizeQuery, [normalizedEvent, compressedEvent, "100"]),
       "updation of ingest_task with normalizedEvent, compressedEvent failed"
     ).to.be.true;
     expect(this.releaseStub.calledOnce, "pg poolclient not released after use").to.be.true;
+  }
+
+  @test
+  @params(
+    {
+      compressedEvent: COMPRESSED_EVENT,
+      normalizedEvent: NORMALIZED_EVENT,
+    },
+    "Should be able to restore original event from normalized and compressed events"
+  )
+  public restoreOriginalEvent({ compressedEvent, normalizedEvent }) {
+    const originalEvent = restoreOriginalEvent(compressedEvent, normalizedEvent);
+    expect(originalEvent).to.deep.equal(ORIGINAL_EVENT);
   }
 }
 
