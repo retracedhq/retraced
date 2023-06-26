2ca44f95f51923140f5d3c6e03d0afd63fb1ea70
ukrocks007 <ukrocks.mehta@gmail.com>
2023-06-26 16:16:06 +0530

    fixes

--- src/common/mmdb.ts
+++ src/common/mmdb.ts
@@ -2,25 +2,30 @@ import { spawn } from "child_process";
 import { ReaderModel, Reader, City } from "@maxmind/geoip2-node";
 import config from "../config";
 import fs from "fs";
+import { logger } from "../logger";
 
 let reader: ReaderModel;
 
 export const execGeoipUpdate = () => {
   const command = "/usr/bin/geoipupdate";
-  const args = ["-f", "/etc/GeoIP.conf", "-d", "/etc/mmdb", "-v"];
+  const args = ["-f", "/etc/GeoIP.conf", "-d", "/etc/mmdb"];
 
   const child = spawn(command, args);
 
   child.stdout.on("data", (data) => {
-    console.log(`stdout: ${data}`);
+    logger.info(`stdout: ${data}`);
   });
 
   child.stderr.on("data", (data) => {
-    console.error(`stderr: ${data}`);
+    logger.info(`stderr: ${data}`);
   });
 
   child.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
+    if (code !== 0) {
+      logger.info(`GeoIPUpdate process exited with code ${code}`);
+    } else {
+      logger.info(`GeoIPUpdate done!`);
+    }
   });
 };
 
