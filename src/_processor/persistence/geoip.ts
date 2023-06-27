53b743d221ac0ba38c02dbe0c02be1e7f26ae54c
Aswin V <vaswin91@gmail.com>
2023-06-27 21:57:45 +0530

    `ingest_task` table optimizations (#835)
    * Remove raw event from `normalized_event`
    
    * Transfer `component` and `version` to `normalized_event`
    
    * Add `team_id` to type
    
    * Condense `original_event` once normalized
    
    * Revert `team_id` addition, stick to new schema
    
    * Remove `fields`, sort list alphabetically
    
    * Impl `condenseOriginalEvent`
    
    * Add migration for new column `compressed_event`
    
    * Fix improper handling of pg connection pool
    
    * Formatting changes
    
    * Impl `restoreOriginalEvent`
    
    * Refactor key list to constant
    
    * Store only the changed fields
    
    * Consolidate sql query
    
    * Fix comment
    
    * Redeem `original_event` from
    `normalized`/ `compressed` columns
    
    * Refactor `geoIP` lookup to use passed `PoolClient`
    
    * Include `_processor` path for test run
    
    * Add `sinon` for mocking pg pool connection
    
    * Use simple strings
    
    * Test for `normalizeEvent` operation
    
    * Include `_processor` tests
    
    * Tweak test param
    
    * Add test error message, assert connection release
    
    * Fix variable naming
    
    * Tweak test error message
    
    * Refactor test and add test for `restoreOriginalEvent`
    
    * Pass constructor args to mock Client
    
    * Fix failing test
    
    * updated epoch of migration files
    
    * Fix test: remove the cleaned up fields
    
    * Remove `raw` from migration call points
    
    * [Tests] Cleanup `raw` in ElasticsearchSaver
    
    * cleanup of raw from gql schema
    
    * updated metrics, retraced-js
    
    ---------
    
    Co-authored-by: Deepak Prabhakara <deepak@boxyhq.com>

--- src/_processor/persistence/geoip.ts
+++ src/_processor/persistence/geoip.ts
@@ -1,32 +1,15 @@
-import getPgPool from "../persistence/pg";
+import { PoolClient } from "pg";
 import config from "../../config";
 
-const pgPool = getPgPool();
+export default async function getLocationByIP(ipAddress, pg: PoolClient) {
+  if (!ipAddress || config.RETRACED_DISABLE_GEOSYNC) {
+    return undefined;
+  }
+  const q = "select * from geoip where network >> $1";
+  const result = await pg.query(q, [ipAddress]);
+  if (result.rowCount > 0) {
+    return result.rows[0];
+  }
 
-export default function getLocationByIP(ipAddress) {
-  return new Promise((resolve, reject) => {
-    if (!ipAddress || config.RETRACED_DISABLE_GEOSYNC) {
-      resolve(undefined);
-      return;
-    }
-
-    pgPool.connect((err, pg, done) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      const q = "select * from geoip where network >> $1";
-      pg.query(q, [ipAddress], (qerr, result) => {
-        done(true);
-        if (qerr) {
-          reject(qerr);
-        } else if (result.rowCount > 0) {
-          resolve(result.rows[0]);
-        } else {
-          resolve(undefined);
-        }
-      });
-    });
-  });
+  return undefined;
 }
