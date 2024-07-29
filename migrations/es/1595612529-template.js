eecd6b0ab8ce3780d4f612d43d8d86454e477bfa
Deepak Prabhakara <deepak@boxyhq.com>
2024-07-29 12:07:43 +0100

    mixed index templates (#1527)

--- migrations/es/1595612529-template.js
+++ migrations/es/1595612529-template.js
@@ -5,90 +5,92 @@ module.exports = () => {
     params: {
       name: "retraced_main",
       body: {
-        template: "retraced*",
-        settings: {
-          analysis: {
-            filter: {
-              autocomplete_filter: {
-                type: "edge_ngram",
-                min_gram: 1,
-                max_gram: 20,
+        index_patterns: ["retraced*"],
+        template: {
+          settings: {
+            analysis: {
+              filter: {
+                autocomplete_filter: {
+                  type: "edge_ngram",
+                  min_gram: 1,
+                  max_gram: 20,
+                },
               },
-            },
-            analyzer: {
-              autocomplete: {
-                type: "custom",
-                tokenizer: "standard",
-                filter: ["lowercase", "autocomplete_filter"],
+              analyzer: {
+                autocomplete: {
+                  type: "custom",
+                  tokenizer: "standard",
+                  filter: ["lowercase", "autocomplete_filter"],
+                },
               },
             },
           },
-        },
-        mappings: {
-          properties: {
-            id: {
-              type: "keyword",
-            },
-            action: {
-              type: "text",
-            },
-            group: {
-              properties: {
-                id: {
-                  type: "keyword",
-                },
-                name: {
-                  type: "text",
+          mappings: {
+            properties: {
+              id: {
+                type: "keyword",
+              },
+              action: {
+                type: "text",
+              },
+              group: {
+                properties: {
+                  id: {
+                    type: "keyword",
+                  },
+                  name: {
+                    type: "text",
+                  },
                 },
               },
-            },
-            actor: {
-              properties: {
-                id: {
-                  type: "keyword",
-                },
-                name: {
-                  type: "text",
-                },
-              },
-            },
-            target: {
-              properties: {
-                id: {
-                  type: "keyword",
-                },
-                name: {
-                  type: "text",
-                },
-                type: {
-                  type: "text",
+              actor: {
+                properties: {
+                  id: {
+                    type: "keyword",
+                  },
+                  name: {
+                    type: "text",
+                  },
                 },
               },
-            },
-            description: {
-              type: "text",
-            },
-            created: {
-              type: "date",
-              format: "epoch_millis",
-            },
-            received: {
-              type: "date",
-              format: "epoch_millis",
-            },
-            canonical_time: {
-              type: "date",
-              format: "epoch_millis",
-            },
-            source_ip: {
-              type: "ip",
-            },
-            raw: {
-              type: "text",
-              index: false,
-            },
-            country: {
-              type: "text",
+              target: {
+                properties: {
+                  id: {
+                    type: "keyword",
+                  },
+                  name: {
+                    type: "text",
+                  },
+                  type: {
+                    type: "text",
+                  },
+                },
+              },
+              description: {
+                type: "text",
+              },
+              created: {
+                type: "date",
+                format: "epoch_millis",
+              },
+              received: {
+                type: "date",
+                format: "epoch_millis",
+              },
+              canonical_time: {
+                type: "date",
+                format: "epoch_millis",
+              },
+              source_ip: {
+                type: "ip",
+              },
+              raw: {
+                type: "text",
+                index: false,
+              },
+              country: {
+                type: "text",
+              },
             },
           },
         },
