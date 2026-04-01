ca94ca28ba25e1213a856d805f216b5b2c8b468e
Andrew Lavery <laverya@umich.edu>
2026-04-01 12:05:15 -0400

    another try

--- src/_db/commands/up/pg.ts
+++ src/_db/commands/up/pg.ts
@@ -8,6 +8,8 @@ import { setupBugsnag } from "../../common";
 setupBugsnag();
 
 import { logger } from "../../../logger";
+import { Client } from "pg";
+
 export const command = "pg";
 export const describe = "migrate postgres database to the current schema";
 
@@ -36,16 +38,25 @@ logger.info("registering handler");
 export const handler = async (argv) => {
   logger.child({up: "pg", schemaPath: argv.schemaPath}).info("beginning handler");
 
-  const connectionString = `tcp://${argv.postgresUser}:${argv.postgresPassword}@${argv.postgresHost}:${argv.postgresPort}/${argv.postgresDatabase}`;
+  const client = new Client({
+    host: argv.postgresHost,
+    port: parseInt(argv.postgresPort, 10),
+    database: argv.postgresDatabase,
+    user: argv.postgresUser,
+    password: argv.postgresPassword,
+  });
 
   try {
+    logger.info("connecting to database");
+    await client.connect();
+
     logger.info("initializing migrator");
     const postgrator = new Postgrator({
       migrationPattern: `${argv.schemaPath}/*`,
       driver: "pg",
       database: argv.postgresDatabase,
-      connectionString,
-    });
+      execQuery: (query) => client.query(query),
+    } as any);
 
     logger.info("executing migration");
     const migrations = await postgrator.migrate();
@@ -54,10 +65,12 @@ export const handler = async (argv) => {
       console.log(chalk.green(m.name));
     });
 
+    await client.end();
     process.exit(0);
   } catch (err) {
     bugsnag.notify(err);
     console.log(chalk.red(err));
+    await client.end().catch(() => {});
     process.exit(1);
   }
 };
